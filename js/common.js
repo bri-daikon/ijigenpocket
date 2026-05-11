9f1704daa218e673f326de19bd953071d7961fef
bri-daikon <kouyahodumi@gmail.com>
2026-05-11 11:30:37 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     nav.id = 'common-nav';
     nav.innerHTML = `
         <div class="nav-container">
-            <a href="index.html" class="nav-logo">
+            <a href="index_true.html" class="nav-logo">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
                 <span>INDEX</span>
             </a>
